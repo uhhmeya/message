940cc644985313ceba28c1802f4cd54016d23a60
ameyamandhare <ameyamandhare17@gmail.com>
2025-05-14 12:38:44 -0400

    git init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <div className="app-container">
+            {
+               <h1>hi</h1>
+            }
+        </div>
+    )
+}
+
+export default App
